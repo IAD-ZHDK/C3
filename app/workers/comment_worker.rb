d8184326a1af323d1864c3f724bffe90c5e3f2ec
Joël Gähwiler <joel.gaehwiler@gmail.com>
2016-02-10 18:34:23 +0100

    do not send comment to commenter

--- app/workers/comment_worker.rb
+++ app/workers/comment_worker.rb
@@ -4,7 +4,9 @@ class CommentWorker
   def perform(comment_id)
     comment = Comment.find(comment_id)
     ParticipantsService.new(comment.clinic).users.each do |user|
-      NotificationMailer.comment_email(comment, user).deliver_later!
+      unless user.id == comment.user_id
+        NotificationMailer.comment_email(comment, user).deliver_later!
+      end
     end
   end
 end
